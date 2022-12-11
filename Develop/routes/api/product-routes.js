e2d1b1db37e2eba1c1afbf21396c087ebf03bbda
David Proto <davidproto@sbcglobal.net>
2022-12-11 13:29:11 -0500

    GET, PUT, DELETE, CREATE routes are working

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -7,7 +7,7 @@ const { Product, Category, Tag, ProductTag } = require("../../models");
 router.get("/", (req, res) => {
   // find all products
   Product.findAll({
-    attributes: ["id", "product_name", "price", "stock", "category_id"],
+    //attributes: ["id", "product_name", "price", "stock", "category_id"],
     include: [
       {
         // be sure to include its associated Category and Tag data
@@ -68,7 +68,12 @@ router.post("/", (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
   */
-  Product.create(req.body)
+  Product.create({
+    product_name: req.body.product_name,
+    price: req.body.price,
+    stock: req.body.stock,
+    category_id: req.body.category_id,
+  })
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
